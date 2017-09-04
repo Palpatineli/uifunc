4e47edf121442ef76126c19bb6d4597b59608a14
Keji Li <mail@keji.li>
2017-09-04 12:41:57 -0400

    working

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='uifunc',
+    version='0.1',
+    packages=['uifunc'],
+    license='',
+    author='Keji Li',
+    author_email='mail@keji.li',
+    extra_require={'wx': 'wx'},
+    description=''
+)
